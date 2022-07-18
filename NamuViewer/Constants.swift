8e694d61141879036c2220bb911fbf4bc62838fa
COMI <admin@wincomi.com>
2022-07-19 02:40:34 +0900

    나무위키 하단의 파워링크 광고 차단 추가

--- NamuViewer/Constants.swift
+++ NamuViewer/Constants.swift
@@ -72,7 +72,11 @@ Array.from(document.getElementsByClassName('wiki-youtube')).forEach( (element) =
 });
 """
 
-		static let adBlock = "var style = document.createElement('style');style.innerHTML = '#search-ad { display: none; }';document.head.appendChild(style);"
+		static let adBlock = """
+var style = document.createElement('style');
+style.innerHTML = '#search-ad, div[class="dSw+obcx"] { display: none; }';
+document.head.appendChild(style);
+"""
 
 		static let disableMemberMenu = """
 document.querySelector('a[title="Member menu"]').remove();
