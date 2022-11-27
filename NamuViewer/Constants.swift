28e115ba667e0ab5dfb9446d0c5ec8720177d6a1
COMI <wincomi@me.com>
2022-11-28 00:16:15 +0900

    앱스토어 심사 문제 수정 - Guideline 2.3.10 - Performance - Accurate Metadata (구글 플레이 버튼 존재) - Guideline 5.1.1(v) - Data Collection and Storage (회원 가입시 탈퇴 불가능)

--- NamuViewer/Constants.swift
+++ NamuViewer/Constants.swift
@@ -31,20 +31,20 @@ enum Constants {
 		enum Selector {
 			static let searchInput = "document.querySelector('nav>form input[type=search]')"
 			static let documentTitle = "document.querySelectorAll('article > div + h1 > a[href]')[0].innerText.trim()"
-			static let tableOfContents = "document.querySelector(\"div[class='HqRYoM-u']\").innerHTML"
+			static let tableOfContents = "document.querySelector(\"div[class='SViB74Q3']\").innerHTML"
 			static let theseedSetting = "document.getElementsByClassName('ion-ios-cog')[0]"
 			static let theseedSettingModal = "document.querySelector(\"div[data-modal='theseed-setting']\")"
 			static let theseedSettingModalDismissButton = "document.querySelector(\"div[data-modal='theseed-setting'] > div > .theseed-fix-dialog > .s > button\")"
 		}
 	}
 
-	enum JavaScript {
-		static let findInPage = "function _namuviewer_find_in_page(text){/*var text=prompt(\"검색할 항목을 입력하세요:\",\"\");*/if(text==null||text.length==0){alert(\"검색 결과가 없습니다.\")}var spans=document.getElementsByClassName(\"labnol\");if(spans){for(var i=0;i<spans.length;i++){spans[i].style.backgroundColor=\"transparent\"}}function searchWithinNode(node,te,len){var pos,skip,spannode,middlebit,endbit,middleclone;skip=0;if(node.nodeType==3){pos=node.data.indexOf(te);if(pos>=0){spannode=document.createElement(\"span\");spannode.setAttribute(\"class\",\"labnol\");spannode.style.backgroundColor=\"yellow\";middlebit=node.splitText(pos);endbit=middlebit.splitText(len);middleclone=middlebit.cloneNode(true);spannode.appendChild(middleclone);middlebit.parentNode.replaceChild(spannode,middlebit);skip=1}}else if(node.nodeType==1&&node.childNodes&&node.tagName.toUpperCase()!=\"SCRIPT\"&&node.tagName.toUpperCase!=\"STYLE\"){for(var child=0;child<node.childNodes.length;++child){child=child+searchWithinNode(node.childNodes[child],te,len)}}return skip}searchWithinNode(document.body,text,text.length)}"
-
-		static let pushStateChanged = "function track(t,s){return function(){return s.apply(this,arguments),t.apply(this,arguments)}}history.pushState=track(history.pushState,function(t,s,e){webkit.messageHandlers.pushStateChanged.postMessage(e)});"
-
-		/// https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
-		static let locationHrefChanged = """
+    enum JavaScript {
+        static let findInPage = "function _namuviewer_find_in_page(text){/*var text=prompt(\"검색할 항목을 입력하세요:\",\"\");*/if(text==null||text.length==0){alert(\"검색 결과가 없습니다.\")}var spans=document.getElementsByClassName(\"labnol\");if(spans){for(var i=0;i<spans.length;i++){spans[i].style.backgroundColor=\"transparent\"}}function searchWithinNode(node,te,len){var pos,skip,spannode,middlebit,endbit,middleclone;skip=0;if(node.nodeType==3){pos=node.data.indexOf(te);if(pos>=0){spannode=document.createElement(\"span\");spannode.setAttribute(\"class\",\"labnol\");spannode.style.backgroundColor=\"yellow\";middlebit=node.splitText(pos);endbit=middlebit.splitText(len);middleclone=middlebit.cloneNode(true);spannode.appendChild(middleclone);middlebit.parentNode.replaceChild(spannode,middlebit);skip=1}}else if(node.nodeType==1&&node.childNodes&&node.tagName.toUpperCase()!=\"SCRIPT\"&&node.tagName.toUpperCase!=\"STYLE\"){for(var child=0;child<node.childNodes.length;++child){child=child+searchWithinNode(node.childNodes[child],te,len)}}return skip}searchWithinNode(document.body,text,text.length)}"
+        
+        static let pushStateChanged = "function track(t,s){return function(){return s.apply(this,arguments),t.apply(this,arguments)}}history.pushState=track(history.pushState,function(t,s,e){webkit.messageHandlers.pushStateChanged.postMessage(e)});"
+        
+        /// https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
+        static let locationHrefChanged = """
 var oldHref = document.location.href;
 window.onload = function() {
  var bodyList = document.querySelector("body");
@@ -65,21 +65,27 @@ window.onload = function() {
  observer.observe(bodyList, config);
 };
 """
-		static let youtubeFix = """
+        static let youtubeFix = """
 Array.from(document.getElementsByClassName('wiki-youtube')).forEach( (element) => {
-	var youtubeId = element.src.substring(element.src.lastIndexOf('/') + 1);
-	element.outerHTML = '<button type="button" class="_namuViewer-youtube-link" style="background: black;display: block;padding: 5px 5px 10px 5px;font-size: 1.1em;color: red;font-weight: bold;text-decoration: none;text-align: center;border: 0;width: 100%" onclick="webkit.messageHandlers.openYoutube.postMessage(\\'' + youtubeId + '\\');"><img src="https://img.youtube.com/vi/' + youtubeId + '/hqdefault.jpg" style="max-width:100%" /><br>▶ Youtube 앱에서 보기</a>';
+ var youtubeId = element.src.substring(element.src.lastIndexOf('/') + 1);
+ element.outerHTML = '<button type="button" class="_namuViewer-youtube-link" style="background: black;display: block;padding: 5px 5px 10px 5px;font-size: 1.1em;color: red;font-weight: bold;text-decoration: none;text-align: center;border: 0;width: 100%" onclick="webkit.messageHandlers.openYoutube.postMessage(\\'' + youtubeId + '\\');"><img src="https://img.youtube.com/vi/' + youtubeId + '/hqdefault.jpg" style="max-width:100%" /><br>▶ Youtube 앱에서 보기</a>';
 });
 """
-
-		static let adBlock = """
+        
+        static let adBlock = """
 var style = document.createElement('style');
 style.innerHTML = '#search-ad, div[class="dSw+obcx"] { display: none; }';
 document.head.appendChild(style);
 """
-
-		static let disableMemberMenu = """
+        
+        static let disableMemberMenu = """
 document.querySelector('a[title="Member menu"]').remove();
 """
-	}
+        
+        static let removeForAppStoreReviews = """
+var style = document.createElement('style');
+style.innerHTML = 'footer > ul:first-child, a[href*=\"/member/signup\"] { display: none; }';
+document.head.appendChild(style);
+"""
+    }
 }
